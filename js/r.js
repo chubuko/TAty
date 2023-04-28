41364517aa9096d6242579b14e57b67cbc902c72
Alexandra Chubukova <113635482+chubuko@users.noreply.github.com>
2023-04-29 01:26:39 +0400

    tt

---
+++ js/r.js
@@ -0,0 +1,16 @@
+// let
+// 		var modal = document.getElementById("myModal");
+
+// 		function open_modal_btn() {
+// 			modal.style.display = "block";
+// 		}
+
+// 		function closeModal() {
+// 			modal.style.display = "none";
+// 		}
+
+// 		window.onclick = function(event) {
+// 			if (event.target == modal) {
+// 				modal.style.display = "none";
+// 			}
+// 		}
